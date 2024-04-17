50bfc0faa713bf1c05a0f8bfed36188ae9c4f36f
Killer3053 <163378224+Killer3053@users.noreply.github.com>
2024-04-18 02:04:47 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# dino
+# Dinosaur-Chrome-Game
+A step by step clone of how to build the Dinosaur game found on Chrome No Internet Screen.
---
+++ js/helpers.js
@@ -0,0 +1,4 @@
+function rand(min, max) {
+	return Math.random() * (max - min) + min;
+}
+<h3[https://killer3053.github.io/UUU/]> find h3>
